d260f0c3816cdab84fe9b362fb74d8ca2ec871d8
nganptt-3490 <phan.thi.trang.ngan@sun-asterisk.com>
2024-08-02 14:23:50 +0700

    Chapter_8 [NaiteiSJP]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
     get "static_pages/home"
     get "static_pages/help"
+    
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     resources :users, only: %i(show)
